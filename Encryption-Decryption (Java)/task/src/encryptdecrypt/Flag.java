764554961ad671b53d437135695e27db30b9a97c
zzuob <af19846@bristol.ac.uk>
2023-04-02 14:05:11 +0100

    Update Flag.java and Main.java to support file IO

--- Encryption-Decryption (Java)/task/src/encryptdecrypt/Flag.java
+++ Encryption-Decryption (Java)/task/src/encryptdecrypt/Flag.java
@@ -1,20 +1,35 @@
 package encryptdecrypt;
 
-import java.util.Objects;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Scanner;
 
 public class Flag {
     static String mode = ""; // "enc" or "dec"
     static int key;
     static String data; // text to shift
+    static boolean fileOutput = false;
+    static String filePath = "";
 
     public static boolean isDecrypt() {
         return mode.equals("dec");
     }
 
-    public static void interactive(){
+    private static String getDataFromFile(String path) {
+        String result = "";
+        try {
+            result = new String(Files.readAllBytes(Path.of(path)));
+        } catch (IOException e) {
+            System.out.println("Error: Cannot read file at " + path);
+            System.exit(-1);
+        }
+        return result;
+    }
+
+    public static void interactive() {
         Scanner scan = new Scanner(System.in);
-        do{
+        do {
             System.out.println("Enter \"enc\" for encryption or \"dec\" for decryption");
             mode = scan.nextLine();
         } while(!(mode.equals("enc") || mode.equals("dec")));
@@ -35,10 +50,12 @@ public class Flag {
 
     public static void cli(String[] args){
         boolean fromFile = false;
-        String filename = "";
         for (String flag: args
              ) {
-            if (flag.equals("-in")) fromFile = true;
+            if (flag.equals("-in")) {
+                fromFile = true;
+                break;
+            }
         }
         for (int i = 0; i < args.length; i++) {
             try {
@@ -46,13 +63,14 @@ public class Flag {
                     case "-mode" -> mode = args[i + 1];
                     case "-key" -> key = Integer.parseInt(args[i + 1]);
                     case "-data" -> data = fromFile ? data : args[i + 1];
-                    case "-in" -> filename = args[i + 1];
+                    case "-in" -> data = getDataFromFile(args[i + 1]);
+                    case "-out" -> {fileOutput = true; filePath = args[i + 1];}
                 }
             } catch (NumberFormatException e) {
-                System.out.println("-key must be an integer: "+e.getMessage());
+                System.out.println("Error: -key must be an integer - "+e.getMessage());
                 System.exit(-1);
             } catch (ArrayIndexOutOfBoundsException e) {
-                System.out.println("missing argument value for "+args[i]);
+                System.out.println("Error: missing argument value for "+args[i]);
                 System.exit(-1);
             }
         }
--- Encryption-Decryption (Java)/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption (Java)/task/src/encryptdecrypt/Main.java
@@ -1,13 +1,21 @@
 package encryptdecrypt;
 
-import java.util.Objects;
-import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
         if (args.length >= 1) {
             Flag.cli(args);
         } else Flag.interactive();
-        System.out.println(Shift.shiftString(Flag.data, Flag.key));
+        String shifted = Shift.shiftString(Flag.data, Flag.key);
+        if (Flag.fileOutput) {
+            try (FileWriter writer = new FileWriter(Flag.filePath)) {
+                writer.write(shifted);
+            } catch (IOException e) {
+                System.out.printf("Error: An exception occurred while writing to \"%s\"", Flag.filePath);
+            }
+        }
+        else System.out.println(shifted);
     }
 }
